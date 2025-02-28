febfd32bad4fed40b39d7d4ec65fb1959ec33d68
Luboš Pechar <lubos.pechar@prirodou.cz>
2025-02-28 14:56:21 +0100

    Přidal jsem dekorátor `login_required` pro vyžadování přihlášení před zobrazením stránky.

--- vlachnovice/pivo/views.py
+++ vlachnovice/pivo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from pivo.models import Tap, Settings
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def home(request):
     try:
         tap = Tap.objects.get(tap=True)
